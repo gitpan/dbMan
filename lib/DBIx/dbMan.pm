5aa1aeec39c25c191c4880ff4a05ff2e4cd7b04e
Milan Sorm <sorm@milansorm.cz>
2003-01-03 15:42:54 +0000

    Import of MILSO/dbMan-0.29 from CPAN.
    gitpan-cpan-distribution: dbMan
    gitpan-cpan-version:      0.29
    gitpan-cpan-path:         MILSO/dbMan-0.29.tar.gz
    gitpan-cpan-author:       MILSO
    gitpan-cpan-maturity:     released

--- lib/DBIx/dbMan.pm
+++ lib/DBIx/dbMan.pm
@@ -7,7 +7,7 @@ use DBIx::dbMan::Lang;
 use DBIx::dbMan::DBI;
 use DBIx::dbMan::MemPool;
 
-$VERSION = '0.28';
+$VERSION = '0.29';
 
 sub new {
 	my $class = shift;
--- lib/DBIx/dbMan/Extension/TotalCount.pm
+++ lib/DBIx/dbMan/Extension/TotalCount.pm
@@ -4,12 +4,12 @@ use strict;
 use vars qw/$VERSION @ISA/;
 use DBIx::dbMan::Extension;
 
-$VERSION = '0.01';
+$VERSION = '0.02';
 @ISA = qw/DBIx::dbMan::Extension/;
 
 1;
 
-sub IDENTIFICATION { return "000001-000073-000001"; }
+sub IDENTIFICATION { return "000001-000073-000002"; }
 
 sub preference { return -30; }
 
@@ -24,7 +24,7 @@ sub handle_action {
 	my ($obj,%action) = @_;
 
 	$action{processed} = 1;
-	if ($action{action} eq 'OUTPUT' and $action{type} eq 'select' and $action{sql} and $obj->{-mempool}->get('total_count')) {
+	if ($action{action} eq 'OUTPUT' and $action{type} eq 'select' and $action{sql} and $obj->{-mempool}->get('total_count') and defined $action{result}) {
 		my $num = scalar @{$action{result}};
 		my $cols = scalar @{$action{fieldnames}};
 		$action{output} .= ($num==0?"No":"Total $num")." line".($num==1?"":"s")." of output in $cols column".($cols==1?"":"s").".\n";
